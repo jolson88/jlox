27358055784e671c86767057cf10ab43306a4d13
Jason Olson <jolson88@outlook.com>
2022-07-15 22:22:49 -0700

    Keyword support

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -5,12 +5,36 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.craftinginterpreters.lox.TokenType.IDENTIFIER;
+
 class Scanner {
+  private static final Map<String, TokenType> keywords;
+
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", TokenType.AND);
+    keywords.put("class", TokenType.CLASS);
+    keywords.put("else", TokenType.ELSE);
+    keywords.put("false", TokenType.FALSE);
+    keywords.put("for", TokenType.FOR);
+    keywords.put("fun", TokenType.FUN);
+    keywords.put("if", TokenType.IF);
+    keywords.put("nil", TokenType.NIL);
+    keywords.put("or", TokenType.OR);
+    keywords.put("print", TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super", TokenType.SUPER);
+    keywords.put("this", TokenType.THIS);
+    keywords.put("true", TokenType.TRUE);
+    keywords.put("var", TokenType.VAR);
+    keywords.put("while", TokenType.WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -52,16 +76,95 @@ class Scanner {
         addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
         break;
 
+      case '/':
+        if (match('/')) {
+          // Comments go until end of line
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(TokenType.SLASH);
+        }
+        break;
+
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace
+        break;
+
       case '\n':
         line++;
         break;
-      case '\r':
-        break;
+
+      case '"': string(); break;
 
       default:
-        Lox.error(line, "Unexpected character: " + c);
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character: " + c);
+        }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance(); // Closing '"'
+
+    // Trim surrounding quotes
+    String value = source.substring(start + 1, current - 1);
+    addToken(TokenType.STRING, value);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  // CLEAN: See in the future if we can eliminate this to only require one-char lookahead
+  // Perhaps an approach similar to Wirth's compilers where there is recognition in the '.' scan
+  // whether next character is digit (for number) or identifier (for method name). That would allow us
+  // to keep the lookahead to a single character and still be able to support both 123.41 and 123.something()
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
   }
 
   private boolean match(char expected) {
@@ -72,12 +175,22 @@ class Scanner {
     return true;
   }
 
-  private boolean isAtEnd() {
-    return current >= source.length();
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
-  private char advance() {
-    return source.charAt(current++);
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
   }
 
   private void addToken(TokenType type) {
